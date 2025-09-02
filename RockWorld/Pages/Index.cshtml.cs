f797c9e52d9cc27884a6502401c8d23307404a32
CBV182 <125630047+CBV182@users.noreply.github.com>
2025-09-02 06:05:47 +0100

    firstComit

--- RockWorld/Pages/Index.cshtml.cs
+++ RockWorld/Pages/Index.cshtml.cs
@@ -18,3 +18,5 @@ namespace RockWorld.Pages
         }
     }
 }
+
+
--- RockWorld/wwwroot/js/site.js
+++ RockWorld/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var x = document.getElementById("myBtn");
+x.addEventListener("mouseover", myFunction);
+x.addEventListener("click", mySecondFunction);
+x.addEventListener("mouseout", myThirdFunction);
+
+function myFunction() {
+    document.getElementById("band").innerHTML += "Nothingham 08.12.2025!<br>";
+}
+
+function mySecondFunction() {
+    document.getElementById("band").innerHTML += "Rock City!<br>";
+}
+
+function myThirdFunction() {
+    document.getElementById("band").innerHTML += "Rock City!<br>";
+}
+
